3a1cbe6d9ddedf5f34917733aaafc300d26ed27e
乐西白 <1327894379@qq.com>
2021-09-04 23:22:12 +0800

    实现预约类构造函数

--- olderFile.cpp
+++ olderFile.cpp
@@ -3,6 +3,95 @@
 //构造函数
 OlderFile::OlderFile()
 {
+	ifstream ifs;
+	ifs.open(OLDER_FILE, ios::in);
+
+	string date;
+	string interval;
+	string stuId;
+	string stuName;
+	string roomId;
+	string status;
+
+	this->size = 0;
+
+	while (ifs >> date && ifs >> interval && ifs >> stuId && ifs >> stuName && ifs >> roomId && ifs >> status)
+	{
+		string key;
+		string value;
+		map<string, string> m;
+
+
+		size_t pos = date.find(':');
+		if (pos != -1)
+		{
+			key = date.substr(0, pos);
+			value = date.substr(pos + 1, date.size() - pos - 1);
+			m.insert(make_pair(key, value));
+		}
+
+		pos = interval.find(':');
+		if (pos != -1)
+		{
+			key = interval.substr(0, pos);
+			value = interval.substr(pos + 1, interval.size() - pos - 1);
+			m.insert(make_pair(key, value));
+		}
+
+		pos = stuId.find(':');
+		if (pos != -1)
+		{
+			key = stuId.substr(0, pos);
+			value = stuId.substr(pos + 1, stuId.size() - pos - 1);
+			m.insert(make_pair(key, value));
+		}
+
+
+		pos = stuName.find(':');
+		if (pos != -1)
+		{
+			key = stuName.substr(0, pos);
+			value = stuName.substr(pos + 1, stuName.size() - pos - 1);
+			m.insert(make_pair(key, value));
+		}
+
+
+		pos = roomId.find(':');
+		if (pos != -1)
+		{
+			key = roomId.substr(0, pos);
+			value = roomId.substr(pos + 1, roomId.size() - pos - 1);
+			m.insert(make_pair(key, value));
+		}
+
+
+		pos = status.find(':');
+		if (pos != -1)
+		{
+			key = status.substr(0, pos);
+			value = status.substr(pos + 1, status.size() - pos - 1);
+			m.insert(make_pair(key, value));
+		}
+
+
+
+		this->older.insert(make_pair(this->size, m));
+		this->size++;
+
+	}
+
+	//测试代码
+	//for (map<int, map<string, string>>::iterator it = this->older.begin(); it != this->older.end();it++)
+	//{
+	//	cout << "key = " << it->first << " value = " << endl;
+	//	for (map<string, string>::iterator mit = it->second.begin(); mit != it->second.end(); mit++)
+	//	{
+	//		cout << mit->first << " " << mit->second << " ";
+	//	}
+	//	cout << endl;
+	//}
+
+	ifs.close();
 
 }
 
