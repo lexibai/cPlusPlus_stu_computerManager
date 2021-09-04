c3f4a902e05d3f89c304cc4d4fe748433345ae3f
乐西白 <1327894379@qq.com>
2021-09-04 23:58:14 +0800

    学生类显示个人预约信息实现

--- student.cpp
+++ student.cpp
@@ -121,6 +121,53 @@ void Student::applyOlder()
 void Student::showMyOlder()
 {
 	OlderFile of;
+
+	if (of.size == 0)
+	{
+		cout << "没有预约记录" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+
+	for (int i = 0; i < of.size; i++)
+	{
+		if (this->studentId == atoi(of.older[i]["stuId"].c_str()))
+		{
+			cout << "本周" << of.older[i]["date"] << " ";
+			cout << "时间段:" << (of.older[i]["interval"] == "1" ? "上午" : "下午") << " ";
+			cout << "预约人id:" << of.older[i]["stuId"] << " ";
+			cout << "预约人:" << of.older[i]["stuName"] << " ";
+			cout << "预约机房号:" << of.older[i]["roomId"] << " ";
+
+			string status = "状态";
+			if (of.older[i]["status"] == "1")
+			{
+				status += "审核中";
+			}
+			else if (of.older[i]["status"] == "2")
+			{
+				status += "预约成功";
+			}
+			else if (of.older[i]["status"] == "-1")
+			{
+				status += "预约失败";
+			}
+			else
+			{
+				status += "预约已取消";
+			}
+
+			cout << status << endl;
+		}
+
+	}
+		
+	system("pause");
+	system("cls");
+	return;
+
+
 }
 
 // 查看所有预约-- - 查看全部预约信息以及预约状态
